4cfc66933564fb6262bc822279325bf78c9ea27b
Darrin_DeYoung <darrindeyoung791@outlook.com>
2025-10-13 23:58:11 +0800

    feat: 修改应用图标

--- app/src/main/java/com/ddy/habitpulse/MainActivity.kt
+++ app/src/main/java/com/ddy/habitpulse/MainActivity.kt
@@ -6,8 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.outlined.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
