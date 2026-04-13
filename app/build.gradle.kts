943960f4b53bf60f13aabc2c5eadaed129458a56
darrindeyoung791 <darrindeyoung791@outlook.com>
2026-04-13 12:41:54 +0800

    chore: 统一用语

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "io.github.darrindeyoung791.habitpulse"
         minSdk = 26
         targetSdk = 36
-        versionCode = 121
-        versionName = "0.5.22-alpha"
+        versionCode = 122
+        versionName = "0.5.23-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resourceConfigurations.addAll(listOf("zh-rCN", "en-rUS"))
