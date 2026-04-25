bb03ead58113d4243676631c53ebf127cf336150
darrindeyoung791 <darrindeyoung791@outlook.com>
2026-04-25 14:58:39 +0800

    docs: 添加vitepress插件，用于缩放预览图片

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,3 +1,4 @@
+/// <reference types="vite/client" />
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
@@ -6,11 +7,32 @@ import Layout from './Layout.vue'
 
 import FeatureCard from '../components/FeatureCard.vue'
 
+import mediumZoom from 'medium-zoom';
+import { onMounted, watch, nextTick } from 'vue';
+import { useRoute } from 'vitepress';
+
+import './style/index.css'
+
+
 
 export default {
   extends: DefaultTheme,
   Layout: () => h(Layout),
   enhanceApp({ app }) {
     app.component('FeatureCard', FeatureCard)
-  }
+  },
+    setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); // 默认
+      mediumZoom('.main img', { background: 'var(--vp-c-bg)' }); // 不显式添加{data-zoomable}的情况下为所有图像启用此功能
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    );
+  },
 } satisfies Theme
