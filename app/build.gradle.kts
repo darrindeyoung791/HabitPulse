f9c8ab5e42b21050a5a29c9b3e0a292f6362434d
darrindeyoung791 <darrindeyoung791@outlook.com>
2026-06-02 08:13:04 +0800

    chore: 移除不必要的文件

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "io.github.darrindeyoung791.habitpulse"
         minSdk = 26
         targetSdk = 36
-        versionCode = 158
-        versionName = "0.8.10-alpha"
+        versionCode = 159
+        versionName = "0.8.11-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resourceConfigurations.addAll(listOf("zh-rCN", "zh-rTW", "zh-rHK", "en-rUS"))
