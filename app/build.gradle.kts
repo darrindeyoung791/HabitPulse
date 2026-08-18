0901c494142bd98e8cf8af24f88f26e656af730e
darrindeyoung791 <darrindeyoung791@outlook.com>
2026-08-18 15:27:39 +0800

    chore: 升级版本至0.8.48-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "io.github.darrindeyoung791.habitpulse"
         minSdk = 26
         targetSdk = 37
-        versionCode = 195
-        versionName = "0.8.47-alpha"
+        versionCode = 196
+        versionName = "0.8.48-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
